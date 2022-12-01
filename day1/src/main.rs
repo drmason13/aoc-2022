f71b7d7a32e41dd16887bbe0ca7a6d2c5ecabef2
drmason13 <drmason13@users.noreply.github.com>
2022-12-01 21:17:46 +0000

    day 1 run both parts in parallel

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,15 +1,60 @@
-fn main() {
-    let input = std::fs::read_to_string("../input/2022/day1.txt").expect("failed to read input");
-    let max_calories = part1(&input);
-    println!("{max_calories}");
+use std::{
+    sync::{
+        mpsc::{self, Sender},
+        Arc,
+    },
+    thread::{self, JoinHandle},
+};
 
-    let max_calories = part2(&input);
-    println!("{max_calories}");
+type Answer = u32;
+type Channel = Sender<Msg<Answer>>;
+
+struct Msg<T>
+where
+    T: Send,
+{
+    part: u8,
+    value: T,
 }
 
-fn part1<T: AsRef<str>>(input: T) -> u32 {
+impl<T> Msg<T>
+where
+    T: Send,
+{
+    fn new(part: u8, value: T) -> Self {
+        Msg { part, value }
+    }
+}
+
+fn main() {
+    let (tx, rx) = mpsc::channel();
+    let input = std::fs::read_to_string("./input/2022/day1.txt").expect("failed to read input");
+    let shared_input = Arc::new(input);
+
+    run_part_threaded(1, shared_input.clone(), part1, tx.clone());
+    run_part_threaded(2, shared_input, part2, tx);
+
+    while let Ok(Msg {
+        value: answer,
+        part,
+    }) = rx.recv()
+    {
+        println!("Got {} for part {}", answer, part);
+    }
+}
+
+fn run_part_threaded<F>(part: u8, input: Arc<String>, solver: F, channel: Channel) -> JoinHandle<()>
+where
+    F: Fn(&str) -> Answer + Send + 'static,
+{
+    thread::spawn(move || {
+        let answer = solver(input.as_ref());
+        channel.send(Msg::new(part, answer)).expect("Send answer");
+    })
+}
+
+fn part1(input: &str) -> u32 {
     let max_calories = input
-        .as_ref()
         .split("\n\n")
         .map(|snacks| {
             snacks
@@ -22,9 +67,8 @@ fn part1<T: AsRef<str>>(input: T) -> u32 {
     max_calories.expect("max calories exist")
 }
 
-fn part2<T: AsRef<str>>(input: T) -> u32 {
+fn part2(input: &str) -> u32 {
     let mut calories = input
-        .as_ref()
         .split("\n\n")
         .map(|snacks| {
             snacks
